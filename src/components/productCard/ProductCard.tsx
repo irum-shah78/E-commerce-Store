f9ccaa61ccb81d9219cfb055aedd8d2ff15c13ad
irum-shah78 <irams4812@gmail.com>
2024-06-27 19:33:40 +0500

    CategoryPage completed

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import heart from "../../assets/icons/heart.png"
-import "../../styles/ProductCard.css"
 
 interface ProductCardProps {
   id: number;
@@ -12,18 +11,18 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ id, title, price, image }) => {
   return (
-    <div className="rounded-2xl w-64 h-64 product-card">
-      <div className="flex items-stretch justify-center mt-6">
-      <img className="w-28 h-28 object-cover" src={image} alt={title} />
-      <div className="rounded-full w-6 h-6 p-1 text-center cursor-pointer favorite-product"><img src={heart} alt="heart" /></div>
+    <div className="rounded-2xl w-64 h-64 border border-gray-400 pt-4 pl-4">
+      <div className="flex items-stretch justify-center">
+        <img className="w-32 h-32 object-cover" src={image} alt={title} />
+        <div className="rounded-full w-6 h-6 p-1 text-center cursor-pointer bg-lightBlue ms-2"><img src={heart} alt="heart" /></div>
       </div>
-      <div className="ms-2 mt-1">
-      <p className="mt-2 font-semibold text-sm">{title}</p>
-      <p className="mt-2 font-semibold text-gray-600">${price}</p>
-      <Link className="mt-4 inline-block bg-blue-500 text-white p-2" to={`/product/${id}`}>
-        View Details
-      </Link>
-    </div>
+      <div className=" mt-1">
+        <p className="mt-2 font-semibold text-xs text-customBlue truncate w-40">{title}</p>
+        <p className="mt-2 font-semibold text-gray-600 text-sm">${price}</p>
+        <Link className="rounded-lg text-white text-xs bg-customYellow p-2 mt-3 inline-block" to={`/product/${id}`}>
+          View Details
+        </Link>
+      </div>
     </div>
   );
 };
--- src/pages/productlistings/CategoryPage.tsx
+++ src/pages/productlistings/CategoryPage.tsx
@@ -1,113 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getCategories, getProductsByCategory } from '../../store/ProductSlice.ts';
-// import { AppDispatch, RootState } from '../../store';
-// import Header from '../../components/header/Header.tsx';
-// import Footer from '../../components/footer/Footer.tsx';
-// import ProductCard from '../../components/productCard/ProductCard.tsx';
-
-// const CategoryPage: React.FC = () => {
-//   const dispatch = useDispatch<AppDispatch>();
-//   const { categories, products, loading, error } = useSelector((state: RootState) => state.products);
-//   const [selectedCategories, setSelectedCategories] = useState<string[]>(() => {
-//     const savedCategories = localStorage.getItem('selectedCategories');
-//     return savedCategories ? JSON.parse(savedCategories) : [];
-//   });
-//   const [allCategoriesChecked, setAllCategoriesChecked] = useState<boolean>(() => {
-//     const savedAllCategoriesChecked = localStorage.getItem('allCategoriesChecked');
-//     return savedAllCategoriesChecked ? JSON.parse(savedAllCategoriesChecked) : false;
-//   });
-
-//   useEffect(() => {
-//     dispatch(getCategories());
-//   }, [dispatch]);
-
-//   useEffect(() => {
-//     if (allCategoriesChecked) {
-//       dispatch(getProductsByCategory('all'));
-//     } else if (selectedCategories.length > 0) {
-//       selectedCategories.forEach(category => {
-//         dispatch(getProductsByCategory(category));
-//       });
-//     }
-//   }, [selectedCategories, allCategoriesChecked, categories, dispatch]);
-
-//   useEffect(() => {
-//     localStorage.setItem('selectedCategories', JSON.stringify(selectedCategories));
-//     localStorage.setItem('allCategoriesChecked', JSON.stringify(allCategoriesChecked));
-//   }, [selectedCategories, allCategoriesChecked]);
-
-//   const handleCategoryChange = (category: string) => {
-//     if (category === 'all') {
-//       setAllCategoriesChecked(!allCategoriesChecked);
-//       setSelectedCategories([]);
-//       if (!allCategoriesChecked) {
-//         dispatch(getProductsByCategory('all'));
-//       }
-//     } else {
-//       setAllCategoriesChecked(false);
-//       if (selectedCategories.includes(category)) {
-//         setSelectedCategories(selectedCategories.filter(cat => cat !== category));
-//       } else {
-//         setSelectedCategories([...selectedCategories, category]);
-//       }
-//     }
-//   };
-
-//   return (
-//     <>
-//       <Header />
-//       <div className="category-page px-32 py-4 p-6 mt-14">
-//         <section className='flex items-center'>
-//         <aside className="categories">
-//           <h2>Categories</h2>
-//           {loading && <p>Loading...</p>}
-//           {error && <p>{error}</p>}
-//           <ul>
-//             <li>
-//               <label>
-//                 <input
-//                   type="checkbox"
-//                   checked={allCategoriesChecked}
-//                   onChange={() => handleCategoryChange('all')}
-//                 />
-//                 All Categories
-//               </label>
-//             </li>
-//             {categories.map((category) => (
-//               <li key={category}>
-//                 <label>
-//                   <input
-//                     type="checkbox"
-//                     checked={selectedCategories.includes(category)}
-//                     onChange={() => handleCategoryChange(category)}
-//                   />
-//                   {category}
-//                 </label>
-//               </li>
-//             ))}
-//           </ul>
-//         </aside>
-
-//         <main className="products">
-//           <p className='text-center font-bold text-2xl text-black'>Products</p>
-//           {loading && <p>Loading...</p>}
-//           {error && <p>{error}</p>}
-//           <div className="product-list flex items-center justify-between">
-//             {products.map((product) => (
-//               <ProductCard key={product.id} {...product} />
-//             ))}
-//           </div>
-//         </main>
-//         </section>
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default CategoryPage;
-
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCategories, getProductsByCategory } from '../../store/ProductSlice.ts';
@@ -115,7 +5,7 @@ import { AppDispatch, RootState } from '../../store';
 import Header from '../../components/header/Header.tsx';
 import Footer from '../../components/footer/Footer.tsx';
 import ProductCard from '../../components/productCard/ProductCard.tsx';
-import '../../styles/CategoryPage.css'; // Import your custom CSS file for CategoryPage styling
+import sale from "../../assets/images/sale.png";
 
 const CategoryPage: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -165,51 +55,223 @@ const CategoryPage: React.FC = () => {
     }
   };
 
+  const handleReset = () => {
+    setSelectedCategories([]);
+    setAllCategoriesChecked(false);
+    dispatch(getProductsByCategory('all'));
+  };
+
   return (
     <>
       <Header />
       <div className="category-page px-32 py-4 p-6 mt-14">
-        <section className='flex items-start'> {/* Ensure items start from the top */}
-          <aside className="categories">
-            <h2>Categories</h2>
-            {loading && <p>Loading...</p>}
+        <section className="flex justify-between">
+          <aside className="w-64 text-sm">
+            {/* Categories */}
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="font-semibold text-customBlue">Categories</h2>
+              <button
+                className="text-gray-500 hover:text-gray-700"
+                onClick={handleReset}>Reset</button>
+            </div>
+            {loading && <p className='text-center'>Loading...</p>}
             {error && <p>{error}</p>}
-            <ul>
+            <ul className="space-y-2">
               <li>
-                <label>
-                  <input
-                    type="checkbox"
-                    checked={allCategoriesChecked}
-                    onChange={() => handleCategoryChange('all')}
-                  />
-                  All Categories
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input
+                      type="checkbox"
+                      checked={allCategoriesChecked}
+                      onChange={() => handleCategoryChange('all')}
+                      className="h-5 w-5 accent-gray-700"
+                    />
+                    <span className="text-gray-800">All Categories</span>
+                  </div>
+                  <span className="text-gray-500">20</span>
                 </label>
               </li>
               {categories.map((category) => (
                 <li key={category}>
-                  <label>
-                    <input
-                      type="checkbox"
-                      checked={selectedCategories.includes(category)}
-                      onChange={() => handleCategoryChange(category)}
-                    />
-                    {category}
+                  <label className="flex items-center justify-between">
+                    <div className="flex items-center space-x-2 cursor-pointer">
+                      <input
+                        type="checkbox"
+                        checked={selectedCategories.includes(category)}
+                        onChange={() => handleCategoryChange(category)}
+                        className="h-5 w-5 accent-gray-700"
+                      />
+                      <span className="text-gray-800">{category}</span>
+                    </div>
+                    <span className="text-gray-500">5</span>
                   </label>
                 </li>
               ))}
             </ul>
+            <hr className='mt-6 border-gray-300' />
+
+
+            {/* Availability */}
+            <h2 className="font-semibold text-customBlue mt-4">Availability</h2>
+            <div className="flex justify-between items-center mb-4">
+              <p className="text-gray-500 hover:text-gray-700">0 selected</p>
+              <button className="text-gray-500 hover:text-gray-700">Reset</button>
+            </div>
+            <ul className="space-y-2 cursor-pointer">
+              <li>
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input
+                      type="checkbox"
+                      className="h-5 w-5 accent-gray-700 rounded"
+                    />
+                    <span className="text-gray-800">In stock</span>
+                  </div>
+                  <span className="text-gray-500">5</span>
+                </label>
+              </li>
+
+              <li>
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input
+                      type="checkbox"
+                      className="h-5 w-5 accent-gray-700 rounded" disabled />
+                    <span className="text-gray-300">Out of stock</span>
+                  </div>
+                  <span className="text-gray-300">0</span>
+                </label>
+              </li>
+            </ul>
+            <hr className='mt-6 border-gray-300' />
+
+
+            {/* Product Type */}
+            <h2 className="font-semibold text-customBlue mt-4">Product type</h2>
+            <div className="flex justify-between items-center mb-4">
+              <p className="text-gray-500 hover:text-gray-700">0 selected</p>
+              <button className="text-gray-500 hover:text-gray-700">Reset</button>
+            </div>
+            <ul className="space-y-2 cursor-pointer">
+              <li>
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input type="checkbox" className="h-5 w-5 accent-gray-700 rounded" />
+                    <span className="text-gray-800">Electronics</span>
+                  </div>
+                  <span className="text-gray-500">5</span>
+                </label>
+              </li>
+
+              <li>
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input type="checkbox" className="h-5 w-5 accent-gray-700 rounded" />
+                    <span className="text-gray-800">Dresses</span>
+                  </div>
+                  <span className="text-gray-500">5</span>
+                </label>
+              </li>
+
+              <li>
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input type="checkbox" className="h-5 w-5 accent-gray-700 rounded" />
+                    <span className="text-gray-800">Jewlery</span>
+                  </div>
+                  <span className="text-gray-500">5</span>
+                </label>
+              </li>
+            </ul>
+            <hr className='mt-6 border-gray-300' />
+
+            {/* Color */}
+            <h2 className="font-semibold text-customBlue mt-4">Color</h2>
+            <div className="flex justify-between items-center mb-4">
+              <p className="text-gray-500 hover:text-gray-700">0 selected</p>
+              <button className="text-gray-500 hover:text-gray-700">Reset</button>
+            </div>
+            <div className='flex gap-4'>
+              <div className='rounded-full bg-yellow-600 p-2'></div>
+              <div className='rounded-full bg-red-600 p-2'></div>
+              <div className='rounded-full bg-teal-950 p-2'></div>
+              <div className='rounded-full bg-gray-500 p-2'></div>
+              <div className='rounded-full bg-green-800 p-2'></div>
+              <div className='rounded-full bg-blue-700 p-2'></div>
+              <div className='rounded-full bg-purple-700 p-2'></div>
+              <div className='rounded-full bg-orange-600 p-2'></div>
+            </div>
+            <hr className='mt-6 border-gray-300' />
+
+            {/* Size */}
+            <h2 className="font-semibold text-customBlue mt-4">Size</h2>
+            <div className="flex justify-between items-center mb-4">
+              <p className="text-gray-500 hover:text-gray-700">0 selected</p>
+              <button className="text-gray-500 hover:text-gray-700">Reset</button>
+            </div>
+            <ul className="space-y-2 cursor-pointer">
+              <li>
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input type="checkbox" className="h-5 w-5 accent-gray-700 rounded" />
+                    <span className="text-gray-800">M</span>
+                  </div>
+                  <span className="text-gray-500">5</span>
+                </label>
+              </li>
+              <li>
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input type="checkbox" className="h-5 w-5 accent-gray-700 rounded" />
+                    <span className="text-gray-800">S</span>
+                  </div>
+                  <span className="text-gray-500">5</span>
+                </label>
+              </li>
+              <li>
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input type="checkbox" className="h-5 w-5 accent-gray-700 rounded" />
+                    <span className="text-gray-800">X</span>
+                  </div>
+                  <span className="text-gray-500">5</span>
+                </label>
+              </li>
+              <li>
+                <label className="flex items-center justify-between">
+                  <div className="flex items-center space-x-2">
+                    <input type="checkbox" className="h-5 w-5 accent-gray-700 rounded" />
+                    <span className="text-gray-800">XX</span>
+                  </div>
+                  <span className="text-gray-500">5</span>
+                </label>
+              </li>
+            </ul>
+            <hr className='mt-6 border-gray-300' />
           </aside>
 
-          <main className="products flex-1">
-            <p className='text-center font-bold text-2xl text-black'>Products</p>
-            {loading && <p>Loading...</p>}
+          <main className="products">
+            {loading && <p className='text-center'>Loading...</p>}
             {error && <p>{error}</p>}
-            <div className="product-list flex flex-wrap justify-between"> {/* Use flexbox for product-list */}
+            <div className="product-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
               {products.map((product) => (
                 <ProductCard key={product.id} {...product} />
               ))}
             </div>
           </main>
+        </section>
+
+        {/* SALES SECTION */}
+        <section>
+          <div className="relative mt-12 mb-12">
+            <img src={sale} alt="sale" className="object-cover w-full h-full" />
+            <div className="absolute inset-y-0 right-0 flex flex-col items-center justify-center space-y-4 p-4 pr-28">
+              <button className="rounded-2xl text-white text-xs px-4 py-2 shop-now-btn">New laptop</button>
+              <p className="text-4xl font-bold" style={{ color: 'rgba(46, 143, 197, 1)' }}>Sale up to 50% off</p>
+              <p className="text-white">12 inch hd display</p>
+              <button className="rounded-2xl text-white text-xs px-4 py-2 shop-now-btn">Shop now</button>
+            </div>
+          </div>
         </section>
       </div>
       <Footer />
@@ -218,4 +280,3 @@ const CategoryPage: React.FC = () => {
 };
 
 export default CategoryPage;
-
